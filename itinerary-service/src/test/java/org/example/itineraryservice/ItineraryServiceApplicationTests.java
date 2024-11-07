3727ebbd04938c37cb5c1639d53616784809c2c8
shimmail <shimmail@126.com>
2024-11-07 15:30:10 +0800

    2024/11/7

---
+++ itinerary-service/src/test/java/org/example/itineraryservice/ItineraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.itineraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineraryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
